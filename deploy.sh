096faaadd5b9afb48ab8a0974114f6ac7c80da03
swapnapoladevops24 <swapnapoladevops24@gmail.com>
2022-06-20 05:08:18 +0530

    deployed logic

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,15 @@ this files contains the deploy
 Deployment script
 hi gm
 add file extra...
-<<<<<<< HEAD
+HEAD
 hi here i am updating the dev 
 Added this line
 added this line for one more practice at master
 
 
-=======
 add filr extra one more practice
 added this file one more practice in development branch
->>>>>>> development
+development
+
+Hello git command - session 4
 
